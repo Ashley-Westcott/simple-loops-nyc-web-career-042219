11f769fd1291e2e36b5f296463b13f7c1ebd0131
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-07 00:23:49 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,7 +13,7 @@ def loop_iterator(number_of_times)
     counter += 1
     phrase = "Welcome to Flatiron School's Web Development Course!"
     puts phrase
-    if counter == number_of_times
+    if counter >= number_of_times
       
     end
 end
